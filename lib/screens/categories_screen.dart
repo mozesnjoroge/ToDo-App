b9851ab8ee6aee775723504a477850c83569e33d
mosesnjoroge19 <njorogemosesngure@gmail.com>
2021-09-16 16:18:21 +0300

    Refactored icons to icon button

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -9,95 +9,95 @@ class CategoriesScreen extends StatefulWidget {
   _CategoriesScreenState createState() => _CategoriesScreenState();
 }
 
-
-
 class _CategoriesScreenState extends State<CategoriesScreen> {
-@override
-void initState() {
-  super.initState();
-  getAllCategories();
-}
+  @override
+  void initState() {
+    super.initState();
+    getAllCategories();
+  }
+
 //controllers
-var _categoryNameController = TextEditingController();
-var _categoryDescriptionController = TextEditingController();
+  var _categoryNameController = TextEditingController();
+  var _categoryDescriptionController = TextEditingController();
 
 //services
-var _category = Category();
-var _categoryService = CategoryServices();
+  var _category = Category();
+  var _categoryService = CategoryServices();
 
 //read services
-List<Category>? _categoryList = <Category>[];
+  List<Category>? _categoryList = <Category>[];
 
-getAllCategories() async {
-  _categoryList = <Category>[];
-  var categories = await _categoryService.readCategory();
-  categories.forEach((category) {
-    setState(() {
-      var categoryModel = Category();
-      categoryModel.id = category['id'];
-      categoryModel.name = category['name'];
-      categoryModel.description = category['description'];
-      _categoryList!.add(categoryModel);
+  getAllCategories() async {
+    _categoryList = <Category>[];
+    var categories = await _categoryService.readCategory();
+    categories.forEach((category) {
+      setState(() {
+        var categoryModel = Category();
+        categoryModel.id = category['id'];
+        categoryModel.name = category['name'];
+        categoryModel.description = category['description'];
+        _categoryList!.add(categoryModel);
+      });
     });
-  });
-}
+  }
 
-_showFormDialog(BuildContext context) {
-  return showDialog(
-    context: context,
-    builder: (context) => AlertDialog(
-      title: Text('Categories Form'),
-      content: SingleChildScrollView(
-        child: Column(
-          children: [
-            TextField(
-              controller: _categoryNameController,
-              decoration: InputDecoration(
-                labelText: 'Category',
-                hintText: 'Enter the category name',
+  _showFormDialog(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Categories Form'),
+        content: SingleChildScrollView(
+          child: Column(
+            children: [
+              TextField(
+                controller: _categoryNameController,
+                decoration: InputDecoration(
+                  labelText: 'Category',
+                  hintText: 'Enter the category name',
+                ),
               ),
-            ),
-            TextField(
-              controller: _categoryDescriptionController,
-              decoration: InputDecoration(
-                labelText: 'Description',
-                hintText: 'Enter the category\'s description',
+              TextField(
+                controller: _categoryDescriptionController,
+                decoration: InputDecoration(
+                  labelText: 'Description',
+                  hintText: 'Enter the category\'s description',
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
+        actions: [
+          OutlinedButton(
+            child: Text(
+              'Save',
+              style: TextStyle(
+                color: Colors.green[900],
+              ),
+            ),
+            onPressed: () async {
+              _category.name = _categoryNameController.text;
+              _category.description = _categoryDescriptionController.text;
+              var result = await _categoryService.insertCategory(_category);
+              print(result);
+              Navigator.of(context).pop();
+            },
+          ),
+          OutlinedButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text(
+              'Cancel',
+              style: TextStyle(
+                color: Colors.black,
+              ),
+            ),
+          ),
+        ],
       ),
-      actions: [
-        OutlinedButton(
-          child: Text(
-            'Save',
-            style: TextStyle(
-              color: Colors.green[900],
-            ),
-          ),
-          onPressed: () async {
-            _category.name = _categoryNameController.text;
-            _category.description = _categoryDescriptionController.text;
-            var result = await _categoryService.insertCategory(_category);
-            print(result);
-            Navigator.of(context).pop();
-          },
-        ),
-        OutlinedButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          child: Text(
-            'Cancel',
-            style: TextStyle(
-              color: Colors.black,
-            ),
-          ),
-        ),
-      ],
-    ),
-  );
-}
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -119,20 +119,33 @@ _showFormDialog(BuildContext context) {
       body: ListView.builder(
           itemCount: _categoryList!.length,
           itemBuilder: (context, index) {
-            return Card(
-              child: ListTile(
-                leading: Icon(Icons.edit),
-                title: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      '${_categoryList![index].name}',
+            return Padding(
+              padding: EdgeInsets.only(top: 10.0, left: 16.0, right: 16.0),
+              child: Card(
+                elevation: 5.0,
+                child: ListTile(
+                  leading: IconButton(
+                    icon: Icon(
+                      Icons.edit,
                     ),
-                    Icon(Icons.delete, color: Colors.red),
-                  ],
-                ),
-                subtitle: Text(
-                  '${_categoryList![index].description}',
+                    onPressed: () {},
+                  ),
+                  title: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '${_categoryList![index].name}',
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.delete),
+                        color: Colors.red,
+                        onPressed: () {},
+                      ),
+                    ],
+                  ),
+                  subtitle: Text(
+                    '${_categoryList![index].description}',
+                  ),
                 ),
               ),
             );
